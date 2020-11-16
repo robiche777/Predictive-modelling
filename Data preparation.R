02d0cb181d83734ba827fc49404759e4e6aa08df
robiche777 <alex.robichaud.77@gmail.com>
2020-11-16 15:50:52 -0500

    Addition of packages needed fo data analysis purpose

--- Data preparation.R
+++ Data preparation.R
@@ -1,7 +1,54 @@
-# This is a code which includ data preparation for my PGA predictive model ####
+# This is a code which include data preparation for my PGA predictive model ####
 
-a <- rnorm(1000)
-plot(a)
+# Installation of packages ####
 
-b <- rgamma(1000,1)
-plot(b)
+# Data manipulations
+install.packages("dplyr")
+install.packages("data.table")
+install.packages("Readr")
+install.packages("tidyr")
+install.packages("stringr")
+install.packages("lubricate")
+
+#data Visualization
+install.packages("ggplot2")
+install.packages("ggvis")
+install.packages("rgl")
+install.packages("htmlwidgets")
+install.packages("lattice")
+
+# Machine learning
+install.packages("mlr")
+install.packages("randomForest")
+install.packages("caret")
+install.packages("e1071")
+install.packages("vcd")
+install.packages("glmnet")
+
+# Loading of the library ####
+# Data manipulations
+library("dplyr")
+library("data.table")
+library("Readr")
+library("tidyr")
+library("stringr")
+library("lubricate")
+
+#data Visualization
+library("ggplot2")
+library("ggvis")
+library("rgl")
+library("htmlwidgets")
+library("lattice")
+
+# Machine learning
+library("mlr")
+library("randomForest")
+library("caret")
+library("e1071")
+library("vcd")
+library("glmnet")
+
+
+
+# Data manipulation
