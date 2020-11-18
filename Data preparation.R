9c2f580f6002fbf7d27640876bf2c4828933881d
robiche777 <alex.robichaud.77@gmail.com>
2020-11-18 12:52:49 -0500

    Creation of the ConvertJSONtoDF function

--- Data preparation.R
+++ Data preparation.R
@@ -9,6 +9,8 @@ install.packages("Readr")
 install.packages("tidyr")
 install.packages("stringr")
 install.packages("lubricate")
+install.packages("rjson")
+install.packages("jsonlite")
 
 #data Visualization
 install.packages("ggplot2")
@@ -33,6 +35,8 @@ library("Readr")
 library("tidyr")
 library("stringr")
 library("lubricate")
+library("rjson")
+library("jsonlite")
 
 #data Visualization
 library("ggplot2")
@@ -50,5 +54,49 @@ library("vcd")
 library("glmnet")
 
 
+# Data importation ####
+Master2020.json <- fromJSON(file = "C:/Users/alexr/Documents/R/Projects/Predictive-modelling/PGA tour data/scorecard.json") #load JSON format to R
 
-# Data manipulation
+# Data manipulation ####
+
+ConvertJsonToDF <- function(x) {
+  
+  #This function allows to reorganize the list format created from the JSON file in a Data frame format
+  
+  # correction needed, find a way to identify which hole the player started to make sure the scores are assigned to the right columns
+  # conditioning could be done on Master2020$scorecards[[i]]$startHole
+  
+  library(plyr)
+  holeId <- ldply(Master2020$scorecards[[x]]$scores, data.frame)[,1]
+  
+  cum.rows <- data.frame()
+  #columnHeader <-c("TournementName", "playerID", "roundID", paste("ScoreHole",holeId, sep = ""), paste("ParHole",holeId, sep = ""), paste("YardageHole",holeId, sep = ""))
+  #colnames(cum.rows) <- scoreHeader
+  
+  for (i in 1:x) { 
+    TournementName <- Master2020$scorecards[[i]]$eventName
+    playerID <- Master2020$scorecards[[i]]$golferId
+    roundID <- Master2020$scorecards[[i]]$round
+    new.row <- c(TournementName, playerID, roundID, as.numeric(ldply(Master2020$scorecards[[i]]$scores, data.frame)[,4]),
+                                               as.numeric(ldply(Master2020$scorecards[[i]]$scores, data.frame)[,3]), 
+                                               as.numeric(ldply(Master2020$scorecards[[i]]$scores, data.frame)[,5]))
+    cum.rows <- rbind(cum.rows, new.row)
+  }
+  
+  columnHeader <-c("TournementName", "playerID", "roundID", paste("ScoreHole",holeId, sep = ""),
+                   paste("ParHole",holeId, sep = ""),
+                   paste("YardageHole",holeId, sep = ""))
+  colnames(cum.rows) <- columnHeader
+  
+  return(cum.rows)
+}
+
+
+number.scorecard <- length(Master2020$scorecards)
+tournement.1 <- ConvertJsonToDF(number.scorecard)
+head(tournement.1)
+tail(tournement.1)
+
+  
+
+
